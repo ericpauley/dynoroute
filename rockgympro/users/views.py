71499aa40e6b9d4f13427db38abd1adee0ceb741
Justin <justin@mysticrealms.net>
2014-02-15 00:24:59 -0500

    changes + route page

--- rockgympro/users/views.py
+++ rockgympro/users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+def home(request):
+	return render(request, "dashboard.html")
+
 def login(request):
 	return render(request, "login.html")
 
 def signup(request):
 	return render(request, "signup.html")
+
+def route(request):
+	return render(request, "route.html")

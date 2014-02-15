c2b443b025aafba74bbb45a6ee735fcde4684dc4
Justin <justin@mysticrealms.net>
2014-02-15 00:37:05 -0500

    add url

--- rockgympro/gyms/urls.py
+++ rockgympro/gyms/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'gyms.views.home', name='home'),
+    url(r'^route/$', 'routes.views.route', name='route')
 )

3a0036b26cd00eeabfd8b6790a8c25eeddcf7f5e
Justin <justin@mysticrealms.net>
2014-02-23 04:12:23 -0500

    script for choosing grade no route add

--- rockgympro/gyms/static/js/script.js
+++ rockgympro/gyms/static/js/script.js
@@ -1,4 +1,23 @@
+var topRopeLabel;
+var topRopeParent;
+var topRopeOptions;
+
+var boulderLabel;
+var boulderParent;
+var boulderOptions;
+
 $(function () {
+
+	topRopeLabel = $("[label='Top Rope']");
+	topRopeParent = topRopeLabel.parent();
+	topRopeOptions = $("[label='Top Rope']").children();
+
+	boulderLabel = $("[label='Bouldering']");
+	boulderParent = boulderLabel.parent();
+	boulderOptions = $("[label='Bouldering']").children();
+
+	$("[label='Top Rope']").detach();
+	$("[label='Top Rope']").children().detach();
 
 	$('#route-date-set').datepicker ()
 
@@ -8,4 +27,21 @@ $(function () {
 	$('#route-color2').simplecolorpicker({
 		picker: true
 	});
-})
+
+});
+
+$("#id_type_1").click(function() {
+	$("[label='Top Rope']").detach();
+	$("[label='Top Rope']").children().detach();
+
+	boulderParent.append(boulderLabel);
+	boulderLabel.append(boulderOptions);
+});
+
+$("#id_type_0").click(function() {
+	$("[label='Bouldering']").detach();
+	$("[label='Bouldering']").children().detach();
+
+	topRopeParent.append(topRopeLabel);
+	topRopeLabel.append(topRopeOptions);
+});

82c33665cdde2b2e29fd6278a7a58ddf402bb7d0
zonedabone <zonedabone@gmail.com>
2014-03-21 23:30:06 -0400

    That thing justin wants

--- rockgympro/users/models.py
+++ rockgympro/users/models.py
@@ -69,7 +69,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         Returns the first_name plus the last_name, with a space in between.
         """
-        return self.name.strip()
+        return self.name.strip() or self.username
 
     def get_short_name(self):
         "Returns the short name for the user."

067420629fb07368724d93f963b38c6ce03c4ee5
zonedabone <zonedabone@gmail.com>
2014-04-19 04:09:37 -0400

    Redirect to HTTPS.

---
+++ gyms/middleware.py
@@ -0,0 +1,7 @@
+django django.shortcuts import redirect
+
+class HttpsRedirectMiddleware(object):
+
+    def process_request(self, request):
+        if request.META.get("HTTP_X_FORWARDED_PROTO", "") == "http":
+            return redirect("https://%s/%s" % (request.get_host(), request.get_full_path()))
--- rockgympro/settings.py
+++ rockgympro/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'gyms.middleware.HttpsRedirectMiddleware'
 )
 
 AUTHENTICATION_BACKENDS = (

31be3cf7b1b8cf645ffb8d74ebadbaf124d38405
zonedabone <zonedabone@gmail.com>
2014-04-19 04:13:34 -0400

    Extra slash is not necessary.

--- gyms/middleware.py
+++ gyms/middleware.py
@@ -4,4 +4,4 @@ class HttpsRedirectMiddleware(object):
 
     def process_request(self, request):
         if request.META.get("HTTP_X_FORWARDED_PROTO", "") == "http":
-            return redirect("https://%s/%s" % (request.get_host(), request.get_full_path()))
+            return redirect("https://%s%s" % (request.get_host(), request.get_full_path()))
